640f343d8cc126a72dacde5b56f97af08e8948ca
роман <tech-79@mail.ru>
2017-02-20 01:24:44 +0500

    Правки для сдачи проекта

--- js/glacy-script.js
+++ js/glacy-script.js
@@ -7,24 +7,13 @@ var closeForm = document.querySelector(".modal-content-close");
 var feedbackForm = popupForm.querySelector("form");
 var login = popupForm.querySelector("[name=username]");
 var email = popupForm.querySelector("[name=user-email]");
-var storage = localStorage.getItem("login");
-
-var way = document.querySelector(".map");
-var popupMap = document.querySelector(".modal-content-map");
-var closeMap = popupMap.querySelector(".close-btn");
 
 link.addEventListener("click", function(event) {
 	event.preventDefault(); 
 	popupForm.classList.add("pop-up-show");
 	popupForm.classList.add("feedback-form-anime");
 	overlay.classList.add("pop-up-show");
-	if(storage) {
-		login.value = storage;
-		email.focus();
-	} else {
-		login.focus();
-	}
-	
+	login.focus();
 });
 
 closeForm.addEventListener("click", function(event) {
@@ -36,7 +25,6 @@ closeForm.addEventListener("click", function(event) {
 	email.classList.remove("modal-content-error");
 });
 
-
 feedbackForm.addEventListener("submit", function(event) {
 	if(!login.value){
 		event.preventDefault(); 
@@ -44,11 +32,8 @@ feedbackForm.addEventListener("submit", function(event) {
 	} else if(!email.value) {
 		event.preventDefault(); 
 		email.classList.add("modal-content-error");
-	} else {
-		localStorage.setItem("login", login.value);
 	}
 });
-
 
 window.addEventListener("keydown", function(event) {
 	if (event.keyCode === 27) {
@@ -57,25 +42,6 @@ window.addEventListener("keydown", function(event) {
 			overlay.classList.remove("pop-up-show");
 			login.classList.remove("modal-content-error");
 			email.classList.remove("modal-content-error");
-		} else if (popupMap.classList.contains("pop-up-show")) {
-			popupMap.classList.remove("pop-up-show");
-			overlay.classList.remove("pop-up-show");
 		}
 	}
-});
-
-
-way.addEventListener("click", function(event) {
-	event.preventDefault(); 
-	popupMap.classList.add("pop-up-show");
-	overlay.classList.add("pop-up-show");
-	closeMap.classList.add("close-btn-modal-map");
-	
-});
-
-closeMap.addEventListener("click", function(event) {
-	event.preventDefault();
-	popupMap.classList.remove("pop-up-show");
-	overlay.classList.remove("pop-up-show");
-	closeMap.classList.remove("close-btn-modal-map");
 });
